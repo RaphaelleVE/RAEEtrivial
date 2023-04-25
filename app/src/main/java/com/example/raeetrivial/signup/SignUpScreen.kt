e89ea4273c9947e50b7b02e50478aa5223186d32
Raphaelle Verdiere <Raphaelle.verdiere-cic@ibm.com>
2023-04-25 09:17:54 +0200

    feat(signupUpdate) : updated ui

--- app/src/main/java/com/example/raeetrivial/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/raeetrivial/signup/SignUpScreen.kt
@@ -4,9 +4,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -16,10 +25,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.raeetrivial.R
 import com.example.raeetrivial.ui.Route
@@ -33,80 +45,93 @@ fun SignUpScreen(navController: NavController) {
     var password by remember { mutableStateOf("") }
 
     Column (modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
+    verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center
-        ) {
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = email,
-                onValueChange = {
-                    email = it
-                },
-                label = {
-                    Text(text = stringResource(R.string.email))
-                },
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrect = false,
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
-                )
+        Icon(
+            painter = painterResource(id = R.drawable.img_trivial_logo),
+            contentDescription = "logo trivial" ,
+            Modifier.fillMaxWidth(),
             )
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = password,
-                onValueChange = {
-                    password = it
-                },
-                label = {
-                    Text(text = stringResource(R.string.password))
-                },
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrect = false,
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
+            Column(Modifier.fillMaxWidth()
+                .padding(30.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    label = {
+                        Text(text = stringResource(R.string.email))
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.None,
+                        autoCorrect = false,
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    )
                 )
-            )
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = password,
-                onValueChange = {
-                    password = it
-                },
-                label = {
-                    Text(text = stringResource(R.string.confirm_password))
-                },
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrect = false,
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    label = {
+                        Text(text = stringResource(R.string.password))
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.None,
+                        autoCorrect = false,
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    )
                 )
-            )
-            Button(modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    //TODO
-                }
-            ) {
-                Text(
-                    text = stringResource(id = R.string.signup),
-                    style = MaterialTheme.typography.titleMedium
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    label = {
+                        Text(text = stringResource(R.string.confirm_password))
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.None,
+                        autoCorrect = false,
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    )
                 )
             }
-            Button(modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    navController.navigate(Route.LOGIN)
+            Column(Modifier.fillMaxWidth()
+                .padding(30.dp),
+                verticalArrangement = Arrangement.Bottom) {
+                Button(modifier = Modifier.fillMaxWidth().padding(5.dp).height(60.dp),
+                    shape = RoundedCornerShape(7.dp),
+                    onClick = {
+                        //TODO
+                    }
+                ) {
+                    Text(
+                        fontSize = (20.sp),
+                        text = stringResource(id = R.string.signup),
+                        style = MaterialTheme.typography.titleMedium
+                    )
                 }
-            ) {
-                Text(
-                    text = stringResource(id = R.string.already_signup_login),
-                    style = MaterialTheme.typography.titleMedium
-                )
+                Button(modifier = Modifier.fillMaxWidth().padding(5.dp).height(60.dp),
+                    shape = RoundedCornerShape(7.dp),
+                    onClick = {
+                        navController.navigate(Route.LOGIN)
+                    }
+                ) {
+                    Text(
+                        fontSize = (20.sp),
+                        text = stringResource(id = R.string.already_signup_login),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
             }
-        }
     }
 }
